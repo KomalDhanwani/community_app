e3386b3dfcb100263510e6c51b49d7aeb94a5ee2
Komal Dhanwani <komal@complitech.net>
2014-08-25 18:10:29 +0530

    initial community updates

---
+++ config/application_config.rb
@@ -0,0 +1,9 @@
+configatron.community_name = "CommunityApp"
+configatron.app_host = "localhost:3000"
+configatron.akismet_key = '123456789'
+configatron.community_tagline = "CommunityEngine Rocks!",
+configatron.community_description =  "The Best Place for People Who Love Communities",
+configatron.support_email =  "komal@complitech.net",
+configatron.meta_description = 'A description of your community for use in META tags.',
+configatron.meta_keywords = 'community diaspora',
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommunityApp::Application.config.secret_token = '03c8a262c3b84adb947f687b5ce8bf8b755075fa3047a47e5ccddf72994fae87e4c0f42bd53d4d32d1cca87bf193380759ea7783c0e64de147421eed31d8c3a1'
