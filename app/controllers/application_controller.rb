8d92fa7d2d36cf53803c40aeb3b0a5123309c585
Komal Dhanwani <komal@complitech.net>
2014-08-25 19:22:29 +0530

    add mysql and other config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate
+  
   protect_from_forgery
+  
+    def authenticate
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "rm" && password == "shocker"
+      end
+    end
+  
 end
--- config/application_config.rb
+++ config/application_config.rb
@@ -5,5 +5,5 @@ configatron.community_tagline = "CommunityEngine Rocks!",
 configatron.community_description =  "The Best Place for People Who Love Communities",
 configatron.support_email =  "komal@complitech.net",
 configatron.meta_description = 'A description of your community for use in META tags.',
-configatron.meta_keywords = 'community diaspora',
+configatron.meta_keywords = 'community diaspora'
 
