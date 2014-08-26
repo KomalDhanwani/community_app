19279fc0551b60b6c2c8dd62d1636cf37f264def
Komal Dhanwani <komal@complitech.net>
2014-08-26 13:51:00 +0530

    add host

--- config/application_config.rb
+++ config/application_config.rb
@@ -1,5 +1,5 @@
 configatron.community_name = "CommunityApp"
-configatron.app_host = "localhost:3000"
+configatron.app_host = "107.170.199.219"
 configatron.akismet_key = '123456789'
 configatron.community_tagline = "CommunityEngine Rocks!",
 configatron.community_description =  "The Best Place for People Who Love Communities",
